3ca5d70fe0986c1ae4c1ceb072c0a41276ad4321
Efren Garnica <efrengarnicapalacios@gmail.com>
2024-10-28 22:28:35 -0600

    Se corrige código para que se puede ver realmente lo que se trea de la API

--- codigo-otros-1/assets/js/app.js
+++ codigo-otros-1/assets/js/app.js
@@ -1,22 +1,28 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name'); //Agregamos 
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+async function displayUser(username) { //agregamos Async
+  try{  //Agregamos try y catch
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  const data = await response.json(); //Agregamos
+  console.log(response); //cambié a response
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;//Se corrigen las comillas
+  $l.textContent = `${data.location}`;
+  
+  }catch(err){
+    handleError(err)
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
